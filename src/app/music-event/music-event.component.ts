bcffa77f34103ef750ecf8d5208ad26b0c6bda63
github username <josephspinelli@yahoo.com>
2019-10-14 16:35:14 -0500

    created music-event component and parent child communication

---
+++ src/app/music-event/music-event.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { MusicEvent } from '../MusicEvent';
+
+@Component({
+  selector: 'app-music-event',
+  templateUrl: './music-event.component.html',
+  styleUrls: ['./music-event.component.scss']
+})
+
+export class MusicEventComponent {
+  @Input() event: MusicEvent;
+}
