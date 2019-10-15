422a02a35cd88d8ebf523a763ebd105726c060d9
github username <josephspinelli@yahoo.com>
2019-10-15 14:29:04 -0500

    imported angular material and styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MusicEventComponent } from './music-event/music-event.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCardModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -9,7 +11,10 @@ import { MusicEventComponent } from './music-event/music-event.component';
     MusicEventComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
