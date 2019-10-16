0f862861e636d2cdd21c7f0eb4989ed3d6f6488e
github username <josephspinelli@yahoo.com>
2019-10-15 23:05:24 -0500

    created login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,25 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MusicEventComponent } from './music-event/music-event.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCardModule } from '@angular/material';
+import { MatButtonModule, MatCardModule, MatInputModule } from '@angular/material';
 import { PriceRangePipe } from './price-range.pipe';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
     AppComponent,
     MusicEventComponent,
-    PriceRangePipe
+    PriceRangePipe,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
