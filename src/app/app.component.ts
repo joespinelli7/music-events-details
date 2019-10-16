881b6ed031dfea24c23f9605fa20bd7d752f1364
github username <josephspinelli@yahoo.com>
2019-10-15 22:26:56 -0500

    created service for music events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,49 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MusicEvent } from './MusicEvent';
+import { MusicEventService } from './music-event.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  events: MusicEvent[] = [
-    {
-      name: 'ACL Festival',
-      date: new Date('10/13/2020'),
-      price: 150,
-      description: 'An annual music festival held in Zilker Park in Austin, Texas on two consecutive three-day weekends.',
-      imageSrc: 'https://image.freepik.com/free-psd/banner-template-summer-festival_23-2148174550.jpg'
-    },
-    {
-      name: 'Lollapalooza',
-      date: new Date('08/02/2020'),
-      price: 200,
-      description: 'Lollapalooza is an annual four-day music festival based in Chicago, Illinois at Grant Park.',
-      imageSrc: 'https://image.freepik.com/free-vector/colorful-music-event-poster-template_1361-1592.jpg'
-    },
-    {
-      name: 'Coachella',
-      date: new Date('04/17/2020'),
-      price: 400,
-      description: 'The Indio desert becomes a fashionable hive of the coolest bands watched by trendy people and celebrities. Near the top of everyone`s bucket list of festivals, the Coachella lineup is a constant source of annual anticipation and a hotbed for musical discussion.',
-      imageSrc: 'https://image.freepik.com/free-photo/crowd-people-dancefloor-with-hands-raised-disco-lights_106386-78.jpg'
-    },
-    {
-      name: 'South By South West',
-      date: new Date('03/16/2020'),
-      price: 100,
-      description: 'Regarded as the ultimate trend-setter and launcher of careers, SXSW is an annual showcase of music, film and interactive highlights enjoyed through performances, showcases, talks, screenings and more.',
-      imageSrc: 'https://img.freepik.com/free-vector/modern-music-event-poster-template_1361-1292.jpg?size=626&ext=jpg'
-    },
-    {
-      name: 'Ultra Music Festival',
-      date: new Date('03/20/2020'),
-      price: 300,
-      description: 'Seen as the ultimate gathering for electronic music fans across the nation and globe, the streets of Miami turn into a bold and pumping party with worshipped DJs providing the summertime soundtrack.',
-      imageSrc: 'https://image.freepik.com/free-vector/music-event-poster-template-with-colorful-shapes_1361-1591.jpg'
-    }
-  ];
+export class AppComponent implements OnInit {
+  musicevents: MusicEvent[];
+
+  constructor(private musicEventService: MusicEventService) { }
+
+  ngOnInit(): void {
+    this.musicevents = this.musicEventService.GetMusicEvents();
+  }
 
   logName(value: string) {
     console.log(value);
