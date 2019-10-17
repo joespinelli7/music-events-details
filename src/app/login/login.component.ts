784a7d36db9c4cc6105639a985d944ebc89f7d2a
github username <josephspinelli@yahoo.com>
2019-10-16 20:11:21 -0500

    more routing for login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,7 @@ export class LoginComponent implements OnInit {
 
   username: string;
   password: string;
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -19,11 +20,13 @@ export class LoginComponent implements OnInit {
     this.loginService.Login(this.username, this.password);
     this.username = '';
     this.password = '';
+    this.router.navigate(['/home']);
   }
 
   Cancel() {
     this.username = '';
     this.password = '';
+    this.router.navigate(['/home']);
   }
 
 }
