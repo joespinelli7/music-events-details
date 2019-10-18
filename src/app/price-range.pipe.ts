4ec327a1f8045cdcfe895b6fba81f547d16fcd84
github username <josephspinelli@yahoo.com>
2019-10-18 13:06:30 -0500

    minor style changes

--- src/app/price-range.pipe.ts
+++ src/app/price-range.pipe.ts
@@ -12,7 +12,7 @@ export class PriceRangePipe implements PipeTransform {
     } else if (value <= 200 && value > 100) {
       return 'Average';
     } else {
-      return 'Cheapest';
+      return 'Cheap';
     }
   }
 
